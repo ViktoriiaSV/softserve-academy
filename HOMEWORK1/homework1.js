9b972060d8b5cec4fa65d4dcfd2a4e08c4beea8e
ViktoriiaSV <viktoriiasytnyk@gmail.com>
2022-07-31 01:59:20 +0300

    task2

---
+++ HOMEWORK1/homework1.js
@@ -0,0 +1,17 @@
+//task 2
+console.log('Sytnyk');
+
+//task 6
+let name = 'Viktoriia';
+let lastName = 'Sytnyk';
+let studyGroup = 'JavaScript Fundamental';
+let age = 34;
+let maritalStatus = true;
+console.log(typeof age, age);
+console.log(typeof maritalStatus, maritalStatus);
+console.log(typeof lastName, lastName);
+
+let hobby;
+console.log(typeof hobby);
+let haveParrot = null;
+console.log(typeof haveParrot);
