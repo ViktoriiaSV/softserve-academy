7d63c194f12fbf46fec4c2fd683bced94feeebb4
ViktoriiaSV <viktoriiasytnyk@gmail.com>
2022-08-04 18:34:41 +0300

    done

--- HOMEWORK2/index.js
+++ HOMEWORK2/index.js
@@ -15,14 +15,15 @@ let res3 = y > x;
 console.log(res3);
 console.log(typeof res3);
 
-let res4 = (x - x) / (y - y) ;
+let res4 = (x - x) / (y - y);
 console.log(res4);
 console.log(typeof res4);
 
 //2
 
 let writeNumber = prompt("Please, write a number");
-let checkNumber = (writeNumber > 0) && (writeNumber % 2 == 0)|| (writeNumber % 7 == 0);
+let checkNumber =
+  (writeNumber > 0 && writeNumber % 2 == 0) || writeNumber % 7 == 0;
 console.log(checkNumber);
 
 //3
@@ -43,27 +44,33 @@ console.log(arr);
 //4
 
 let cities = ["Rome", "Lviv", "Warsaw"];
-let citiesNew = cities.join('*');
+let citiesNew = cities.join("*");
 console.log(citiesNew);
 
 //5
 
-let isAdult =  prompt("Please tell, if you are 18 years old?");
+let isAdult = prompt("Please tell, if you are 18 years old?");
 if (isAdult == "yes") {
-    alert("You have reached the age of majority!")
+  alert("You have reached the age of majority!");
 } else {
-    alert("You are still too young!")
-};
+  alert("You are still too young!");
+}
 
 //6
 
-let firstSide = parseFloat(prompt("Please write length of the side of the triangle"));
-let secondSide = parseFloat(prompt("Please write length of the side of the triangle"));
-let thirdSide = parseFloat(prompt("Please write length of the side of the triangle"));
+let firstSide = parseFloat(
+  prompt("Please write length of the side of the triangle")
+);
+let secondSide = parseFloat(
+  prompt("Please write length of the side of the triangle")
+);
+let thirdSide = parseFloat(
+  prompt("Please write length of the side of the triangle")
+);
 
 if (firstSide < 0 || secondSide < 0 || thirdSide < 0) {
-    alert("Incorrect data")
-}; 
+  alert("Incorrect data");
+}
 
 let semiPerimeter = (firstSide + secondSide + thirdSide) / 2;
 let areaTriangle = Math.sqrt(
@@ -72,18 +79,52 @@ let areaTriangle = Math.sqrt(
     (semiPerimeter - secondSide) *
     (semiPerimeter - thirdSide)
 );
-console.log("Area of triangle: " + (Math.round( areaTriangle * 1000 ) / 1000));
+console.log("Area of triangle: " + Math.round(areaTriangle * 1000) / 1000);
 
- if ((firstSide*firstSide) === (secondSide*secondSide) + (thirdSide*thirdSide)) {
-    console.log("Triangle is right angle")
- } else if ((secondSide*secondSide) === (firstSide*firstSide) + (thirdSide*thirdSide)) {
-    console.log("Triangle is right angle")
- } else if ((thirdSide*thirdSide) === (secondSide*secondSide) + (firstSide*firstSide)) {
-    console.log("Triangle is right angle")
- } else {
-    console.log("Triangle is NOT right angle")
- };
+if (firstSide * firstSide === secondSide * secondSide + thirdSide * thirdSide) {
+  console.log("Triangle is right angle");
+} else if (
+  secondSide * secondSide ===
+  firstSide * firstSide + thirdSide * thirdSide
+) {
+  console.log("Triangle is right angle");
+} else if (
+  thirdSide * thirdSide ===
+  secondSide * secondSide + firstSide * firstSide
+) {
+  console.log("Triangle is right angle");
+} else {
+  console.log("Triangle is NOT right angle");
+}
 
- //7
+//7
 
+let hour = new Date().getHours();
+console.log(hour);
 
+if (hour > 23 && hour < 5) {
+  alert("Good night");
+} else if (hour >= 5 && hour < 11) {
+  alert("Good morning");
+} else if (hour >= 11 && hour < 17) {
+  alert("Good day");
+} else if (hour >= 17 && hour <= 23) {
+  alert("Good evening");
+}
+
+switch (true) {
+  case hour > 23 && hour < 5:
+    alert("Good night");
+    break;
+  case hour >= 5 && hour < 11:
+    alert("Good morning");
+    break;
+  case hour >= 11 && hour < 17:
+    alert("Good day");
+    break;
+  case hour >= 17 && hour <= 23:
+    alert("Good evening");
+    break;
+  default:
+    alert("Error");
+}
